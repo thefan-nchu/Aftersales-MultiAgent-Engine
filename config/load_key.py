3ac8a7ec19ce774b9ca52586215ff640a83652ca
thefan <85053050+thefan-nchu@users.noreply.github.com>
2026-04-19 16:36:59 +0800

    Update load_key.py

--- config/load_key.py
+++ config/load_key.py
@@ -2,9 +2,8 @@ import os
 
 
 def load_key():
-    # 设置阿里云 DashScope 的 Key
-    os.environ["DASHSCOPE_API_KEY"] = "sk-4500e056fb41401da4a1f3660639bb67"
-    os.environ["REDIS_PASSWORD"] = "wodemima"
-    os.environ["TONGYI_API_KEY"] = "sk-4500e056fb41401da4a1f3660639bb67"
-    os.environ["OPENAI_API_KEY"] = "sk-xxQdZhffEkSiVxlHXM2RR5PeC1cD9yB9bgVsJkr6Hsh31Pll"
-    os.environ["REDIS_URL"] = "redis://:wodemima@localhost:6379/0"
+    os.environ["DASHSCOPE_API_KEY"] = "sk-**"
+    os.environ["REDIS_PASSWORD"] = "**"
+    os.environ["TONGYI_API_KEY"] = "sk-**"
+    os.environ["OPENAI_API_KEY"] = "sk-**"
+    os.environ["REDIS_URL"] = "redis://:**@localhost:6379/0"
